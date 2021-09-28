734300827ad90670922fe59bb5edaf3a01f81045
Valentina.Isaeva <vaaalentinaaaa@mail.ru>
2021-09-28 18:07:59 +0300

    сценарий создания новой роли

---
+++ test_add_role.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+class TestAddRole(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(30)
+    
+    def test_add_role(self):
+        driver = self.driver
+        # Label: Test
+        # ERROR: Caught exception [ERROR: Unsupported command [resizeWindow | 1280,609 | ]]
+        driver.get("http://10.70.36.234/#/login")
+        driver.find_element_by_css_selector("div.mx-auto.my-12.card.v-card.v-sheet.theme--light").click()
+        driver.find_element_by_id("auth-login-field-username").click()
+        driver.find_element_by_id("auth-login-field-username").clear()
+        driver.find_element_by_id("auth-login-field-username").send_keys("test")
+        driver.find_element_by_id("auth-login-field-password").clear()
+        driver.find_element_by_id("auth-login-field-password").send_keys("123")
+        driver.find_element_by_id("auth-login-field-password").clear()
+        driver.find_element_by_id("auth-login-field-password").send_keys("123")
+        driver.find_element_by_id("auth-login-btn-submit").click()
+        driver.find_element_by_xpath("//section[@id='main-wrapper']/nav/div/div[2]/div[2]/div[2]/div/div/div[2]").click()
+        driver.find_element_by_css_selector("#main-layout-navigation-user_roles > div.v-list-item__content > div.v-list-item__title").click()
+        driver.find_element_by_css_selector("button.v-btn.v-btn--is-elevated.v-btn--has-bg.theme--light.v-size--default > span.v-btn__content").click()
+        driver.find_element_by_id("input-469").click()
+        driver.find_element_by_css_selector("div.col.col-12 > div.row").click()
+        driver.find_element_by_id("input-469").clear()
+        driver.find_element_by_id("input-469").send_keys("test")
+        driver.find_element_by_css_selector("button.primary.v-btn.v-btn--is-elevated.v-btn--has-bg.theme--light.v-size--default > span.v-btn__content").click()
+        driver.find_element_by_id("main-layout-navigation-btn-logout").click()
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+
+
+    
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
